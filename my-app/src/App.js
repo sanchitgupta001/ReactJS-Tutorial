4d6db646a499b813a794bf3eeac51cc789feded4
sanchitgupta001 <sanchit.gupta@sprinklr.com>
2017-12-09 22:33:08 +0530

    Added LifecyleMethods.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,48 +6,11 @@ import ReactDOM from 'react-dom';
 class App extends Component{
   constructor(){
     super();
-    this.state = {val: 0};
-    this.update = this.update.bind(this);
-  }
-
-  update(){
-    this.setState({val: this.state.val + 1})
-  }
-  componentWillMount(){
-    console.log("componentWillMount");
   }
   render(){
-    console.log('render');
-    return < button onClick={this.update}>{this.state.val}</button>
+    return <h1>Hello There!</h1>
   }
-  componentDidMount(){
-    console.log("componentDidMount");
-  }
-  // componentWillReceiveProps (When component receives new props)
-  // shouldComponentUpdate (Before rendering, after receiving new props or state)
-  // componentWillUpdate (Before rendering, after receiving new props or state)
-  // componentDidUpdate (After component's updates are flushed to DOM)
-  componentWillUnmount(){
-    console.log("componentWillUnmount");
-  }
+
 }
 
-class Wrapper extends Component{
-  mount(){
-    ReactDOM.render(<App />, document.getElementById('a'));
-  }
-  unmount(){
-    ReactDOM.unmountComponentAtNode(document.getElementById('a'))
-  }
-  render(){
-    return(
-      <div>
-        <button onClick={this.mount.bind(this)}>Mount</button>
-        <button onClick={this.unmount.bind(this)}>Unmount</button>
-        <div id="a"></div>
-      </div>
-    );
-  }
-}
-
-export default Wrapper;
+export default App;
